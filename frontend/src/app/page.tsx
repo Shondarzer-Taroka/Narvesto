1163eaddf0bc2fb154b7e8abd76d131439f47a48
shondarzertaroka95 <shondarzertaroka95@gmail.com>
2025-03-03 17:28:11 +0600

    faq

--- frontend/src/app/page.tsx
+++ frontend/src/app/page.tsx
@@ -2,7 +2,7 @@ import ShortStats from "@/components/home/ShortStats";
 import bgImg from '@/asset/image/Useful/Rectangle 5985.png'
 import InvestmentOpportunity from "@/components/home/InvestmentOpportunity";
 import WhyChoseUs from "@/components/home/WhyChoseUs";
-import FAQ from "@/components/home/Faq";
+
 import WhatWeOffer from "@/components/home/WhatWeOffer";
 import FAQAccordion from "@/components/home/Accordion";
 
